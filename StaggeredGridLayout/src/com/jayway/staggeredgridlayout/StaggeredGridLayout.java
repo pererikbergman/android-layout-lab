787e8038d67a4430556dad6439cb9df25d3a657f
Per-Erik Bergman <per-erik.bergman@jayway.com>
2014-05-03 18:25:09 +0200

    Updated headers.
    Conflicts:
    	SquareGridLayout/src/com/jayway/squaregridlayout/SquareGridLayout.java
    	StaggeredGridLayout/src/com/jayway/staggeredgridlayout/StaggeredGridLayout.java

--- StaggeredGridLayout/src/com/jayway/staggeredgridlayout/StaggeredGridLayout.java
+++ StaggeredGridLayout/src/com/jayway/staggeredgridlayout/StaggeredGridLayout.java
@@ -1,3 +1,19 @@
+/*
+ * (C) Copyright 2014 Jayway AB (http://www.jayway.com/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.jayway.staggeredgridlayout;
 
 import android.content.Context;
@@ -67,7 +83,7 @@ public class StaggeredGridLayout extends ViewGroup {
     }
 
     /**
-     * Compute the size of itself and it’s children.
+     * Compute the size of itself and its children.
      *
      * {@inheritDoc}
      */
@@ -104,7 +120,7 @@ public class StaggeredGridLayout extends ViewGroup {
 
 
     /**
-     * Compute the bounds of it’s children for drawing.
+     * Compute the bounds of its children for drawing.
      *
      * {@inheritDoc}
      */
