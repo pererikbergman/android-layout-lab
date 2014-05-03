827d4f0d0b16dc2e64c4fa2ee204ed2a9f64c135
Per-Erik Bergman <per-erik.bergman@jayway.com>
2014-05-03 18:32:48 +0200

    Updated the headers.

--- Android-Layout-Lab/SquareGridLayout/src/main/java/com/jayway/squaregridlayout/SquareGridLayout.java
+++ Android-Layout-Lab/SquareGridLayout/src/main/java/com/jayway/squaregridlayout/SquareGridLayout.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2013 Jayway AB (http://www.jayway.com/).
+ * (C) Copyright 2014 Jayway AB (http://www.jayway.com/).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
